a9cd2c4cc75d6de9bbbc87deb55d7be90f509fd6
Gualtiero Frigerio <gualtiero.frigerio@epress.it>
2019-03-14 16:31:48 +0100

    first commit

---
+++ GFPDFViewer/Classes/GFPDFDocumentProvider.swift
@@ -0,0 +1,8 @@
+//
+//  GFPDFDocumentProvider.swift
+//  GFPDFViewer
+//
+//  Created by Gualtiero Frigerio on 13/03/2019.
+//
+
+import Foundation
---
+++ GFPDFViewer/Classes/GFPDFTiledView.swift
@@ -0,0 +1,20 @@
+//
+//  GFPDFTiledView.swift
+//  GFPDFViewer
+//
+//  Created by Gualtiero Frigerio on 13/03/2019.
+//
+
+import UIKit
+
+class GFPDFTiledView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
